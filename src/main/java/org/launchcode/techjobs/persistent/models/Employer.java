f96dbe21593e2ffc5e74fd8aa5fbd1844a31b560
chris <selfawarecpu21@gmail.com>
2022-11-09 11:38:05 -0600

    Part1 complete

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+
+    @NotBlank
+    @Size(min=3, max=30)
+    private String location;
+
+
+
+    public Employer(){
+
+    }
+
+
+
+    public String getLocation() {
+        return location;
+    }
 
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @NotBlank
+    @Size(min=10, max=250)
+    private String description;
+
+
+    public Skill(){
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
 
 }
